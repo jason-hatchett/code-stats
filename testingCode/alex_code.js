3e27bdb366f060d634176da92ad15bc2a11d1160
Jason Hatchett <jason@hatchett.net>
2015-02-17 12:12:45 -0600

    Clean up git repo

--- testingCode/alex_code.js
+++
@@ -1,8 +0,0 @@
-function charSum (str) {
-  //var a = str.replace(/[^0-9]/g, '')
-  var sum = 0
-  for (i=0; i<str.length; i++) {
-    sum += parseInt(str[i]) || 0
-  }
-  return sum
-}
--- testingCode/brian_code.js
+++
@@ -1,9 +0,0 @@
-function charSum (str) {
-  var sum = 0
-  for (i = 0; i < str.length; i++) {
-    if (parseInt(str[i])) {
-      sum += parseInt(str[i])
-      }
-    }
-  return sum
-}
