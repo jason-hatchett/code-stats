01ca2caeb58398a411407f36566e43c2ba653901
Jason Hatchett <jason@hatchett.net>
2015-02-17 15:10:32 -0600

    Highlights.js added and no cdns used

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-  angular.module('codeStats', ['codeStats.controllers', 'ui.bootstrap']);
+  angular.module('codeStats', ['codeStats.controllers', 'ui.bootstrap', 'hljs']);
 
   angular.module('codeStats.services', []);
   angular.module('codeStats.controllers', ['codeStats.services', 'highcharts-ng']);
--- app/scripts/controllers/statCtrl.js
+++ app/scripts/controllers/statCtrl.js
@@ -127,7 +127,7 @@
               }
             })
           }
-          
+          hljs.initHighlightingOnLoad();
           callback()
         });
         this.snippet = {};
